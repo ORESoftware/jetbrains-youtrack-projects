60a12140eefaabdb2f2f5df0086822340afd309b
Olegzandr VD <alex@oresoftware.com>
2017-11-26 19:16:45 -0800

    init

---
+++ auto-suggest-failure/2.test.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+import suman, { s } from 'suman';
+const { Test } = suman.init(module);
+
+
+Test.create(function (it) {
+
+  //fooagaoigeon
+
+  it.define('help')
+    .description('zoom')
+    .cb(true)
+    .run(function () {
+
+    });
+
+
+  it('works', (t: s.ITestCaseParam) => {
+
+  });
+
+
+});
