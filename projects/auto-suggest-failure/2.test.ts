bad4d907d243c27a8bfc2f9cb9e523e5833284a0
Olegzandr Denman <alex@oresoftware.com>
2017-11-27 20:19:27 -0800

    pdev:set

--- projects/auto-suggest-failure/2.test.ts
+++ projects/auto-suggest-failure/2.test.ts
@@ -7,12 +7,14 @@ const { Test } = suman.init(module);
 Test.create(function (it: s.ItFn) {
 
   //fooagaoigeon
-  
+
   
 
   it.define('help')
     .cb(true)
-    .run();
+    .run(function(){
+
+    });
 
 
   it('works', (t: s.ITestCaseParam) => {
