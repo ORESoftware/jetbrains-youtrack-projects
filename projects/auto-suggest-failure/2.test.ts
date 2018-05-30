47c88893e9cfd172b0a8c442fb71821f6fafd263
Olegzandr VD <alex@oresoftware.com>
2018-05-29 21:33:05 -0700

    pdev:set

--- projects/auto-suggest-failure/2.test.ts
+++ projects/auto-suggest-failure/2.test.ts
@@ -1,23 +1,18 @@
 'use strict';
 
-import suman, { s } from 'suman';
-const { Test } = suman.init(module);
-
+import suman, {s} from 'suman';
+const {Test} = suman.init(module);
 
 Test.create(function (it: s.ItFn) {
-
+  
   //fooagaoigeon
   
-  
-
   it.define('help')
-    .cb(true)
-    .run();
-
-
+  .cb(true)
+  .run();
+  
   it('works', (t: s.ITestCaseParam) => {
-
+  
   });
-
-
+  
 });
